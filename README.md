1f72180a725d0562c2d42c69e9f2869c28adf3ec
diegovalemoreno <diegosvm@hotmail.com>
2018-12-21 09:58:52 -0200

    Criação do README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# Cartão Pessoal NPX - Diego Moreno :floppy_disk:
+
+<p align="center">
+  <img src="https://i.ibb.co/rknPY7G/Cartao-Pessoal-Diego-Moreno.jpg"/>  
+</p>
+
+Desenvolvimento de um package que simula um cartão pessoal. O exemplo desenvolvido aqui foi baseado 
+no repositório do ilustre **[Tierney Cyren](@bitandbang)**
+
+No caso aqui, estou usando os meus dados para criar o meu Cartão Pessoal. O pacote, que nesse caso é o meu cartão pessoal, encontra-se disponível no site do **npmjs** pelo link: 
+
+- Cartão Pessoal Glaucia Lemos: **[diegomoreno](https://www.npmjs.com/package/diegomoreno)**
+
+Para instalar o package do meu cartão pessoal, bastam executar o seguinte comando abaixo:
+
+```
+> npm install diegomoreno
+
+```
--- bin/card.js
+++ bin/card.js
@@ -20,13 +20,11 @@ const data = {
   twitter: chalk.cyan('https://twitter.com/@diegovalemoreno'),
   github: chalk.cyan('https://github.com/diegovalemoreno'),
   linkedin: chalk.cyan('https://www.linkedin.com/in/diegovalemoreno/'),
-//   web: chalk.cyan('https://code4coders.wordpress.com/'),
   npx: chalk.white('npx diegomoreno'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
   labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-//   labelWeb: chalk.white.bold('       Web:'),
   labelCard: chalk.white.bold('      Card:')
 }
 
@@ -37,11 +35,8 @@ const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-// const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + newline + carding
-// const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
-
 console.log(chalk.green(boxen(output, options)))
